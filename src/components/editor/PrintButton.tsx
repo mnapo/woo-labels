6c0130adb709abc87e64a5d961823ecc71d543c3
mnapo <napolimatiase@gmail.com>
2026-07-29 02:55:53 -0300

    create PrintButton

---
+++ src/components/editor/PrintButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function PrintButton() {
+  return (
+    <button
+      onClick={() => window.print()}
+      className="..."
+    >
+      Imprimir
+    </button>
+  );
+}
