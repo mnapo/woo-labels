d60af59c796005c0c649204c37b7801ef1d1ff52
mnapo <napolimatiase@gmail.com>
2026-07-29 01:40:28 -0300

    create useProducts

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Product } from "@/types/product";
+
+export function useProducts() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch("/api/products");
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
+        const data = await response.json();
+
+        setProducts(data);
+      } catch (err) {
+        setError(
+          err instanceof Error
+            ? err.message
+            : "Unknown error"
+        );
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return {
+    products,
+    loading,
+    error,
+  };
+}
