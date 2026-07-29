5c804b9f0b4ceea69e7147dc5f12a8092e76d96e
mnapo <napolimatiase@gmail.com>
2026-07-29 00:48:54 -0300

    refactor to show LabelCell's instead of numeric placeholders

--- src/components/LabelGrid.tsx
+++ src/components/LabelGrid.tsx
@@ -1,4 +1,5 @@
 import { SheetPreset } from "@/types/sheet";
+import LabelCell from "./editor/LabelCell";
 
 interface Props {
   preset: SheetPreset;
@@ -8,33 +9,31 @@ export default function LabelGrid({
   preset,
 }: Props) {
 
-  const total = preset.rows * preset.cols;
+  const total =
+    preset.rows * preset.cols;
+
 
   return (
     <div
-      className="grid w-full max-w-3xl"
+      className="grid w-full max-w-4xl"
       style={{
         gridTemplateColumns:
           `repeat(${preset.cols}, 1fr)`
       }}
     >
-      {Array.from({ length: total }).map(
-        (_, index) => (
-          <div
-            key={index}
-            className="
-              aspect-[2/1]
-              border
-              flex
-              items-center
-              justify-center
-              text-sm
-            "
-          >
-            {index + 1}
-          </div>
-        )
-      )}
+      {Array.from({
+        length: total
+      }).map((_, index)=>(
+        <LabelCell
+          key={index}
+          onClick={()=>{
+            console.log(
+              "cell",
+              index
+            );
+          }}
+        />
+      ))}
     </div>
   );
 }
