265ce0f3a8490a17348e9d2c8c567f34bf284b41
mnapo <napolimatiase@gmail.com>
2026-07-29 03:09:27 -0300

    update presets

--- src/lib/presets.ts
+++ src/lib/presets.ts
@@ -2,21 +2,21 @@ import { SheetPreset } from "@/types/sheet";
 
 export const SHEET_PRESETS: SheetPreset[] = [
   {
-    id: "12x3",
-    name: "12 x 3",
-    rows: 12,
+    id: "9x3",
+    name: "9 x 3",
+    rows: 9,
     cols: 3,
   },
   {
-    id: "15x4",
-    name: "15 x 4",
-    rows: 15,
+    id: "12x4",
+    name: "12 x 4",
+    rows: 12,
     cols: 4,
   },
   {
-    id: "18x5",
-    name: "18 x 5",
-    rows: 18,
+    id: "15x5",
+    name: "15 x 5",
+    rows: 15,
     cols: 5,
   },
 ];
