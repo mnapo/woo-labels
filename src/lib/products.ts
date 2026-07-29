11a67caec0282fc35df341d8e6d6d59c776666c6
mnapo <napolimatiase@gmail.com>
2026-07-29 00:58:24 -0300

    add mock products

---
+++ src/lib/products.ts
@@ -0,0 +1,24 @@
+import { Product } from "@/types/product";
+
+export const MOCK_PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: "Harina 000",
+    price: "1500",
+  },
+  {
+    id: 2,
+    name: "Azúcar",
+    price: "1200",
+  },
+  {
+    id: 3,
+    name: "Coca Cola 2.25L",
+    price: "3200",
+  },
+  {
+    id: 4,
+    name: "Yerba Mate",
+    price: "4500",
+  },
+];
