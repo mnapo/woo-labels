a5b066afb8fa17325865260014bf3d2c8479dfa1
mnapo <napolimatiase@gmail.com>
2026-07-29 00:40:24 -0300

    add grid presets

---
+++ src/lib/presets.ts
@@ -0,0 +1,22 @@
+import { SheetPreset } from "@/types/sheet";
+
+export const SHEET_PRESETS: SheetPreset[] = [
+  {
+    id: "12x3",
+    name: "12 x 3",
+    rows: 12,
+    cols: 3,
+  },
+  {
+    id: "15x4",
+    name: "15 x 4",
+    rows: 15,
+    cols: 4,
+  },
+  {
+    id: "18x5",
+    name: "18 x 5",
+    rows: 18,
+    cols: 5,
+  },
+];
