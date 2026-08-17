b3d959051eb2fe44b24b75e08f0da5108c46dbce
Matías Nápoli <napolimatiase@gmail.com>
2026-08-17 13:30:40 -0300

    add borderStyle and borderWidth properties

--- src/types/label.ts
+++ src/types/label.ts
@@ -2,4 +2,6 @@ export type LabelConfig = {
   fontSize: "small" | "medium" | "large";
   layout: "vertical" | "horizontal";
   showBarcode: boolean;
+  borderStyle: "solid" | "dashed" | "none";
+  borderWidth: "thin" | "medium" | "thick";
 };
